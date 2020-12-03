2bd10f3d5f3108b8ef960b7a32f041622ac93a42
Juulia Devadze <jd@ishoj>
2020-12-03 13:32:17 +0100

    JYU-559-64412 Wellcome block on user page

--- public_html/sites/all/themes/ishoj/templates/user-profile.tpl.php
+++ public_html/sites/all/themes/ishoj/templates/user-profile.tpl.php
@@ -28,28 +28,12 @@
 
                   ?>
                   </h1>
+                  <?php
+                    $block = module_invoke('block', 'block_view', '1');
+                    print render($block['content']);
+                    ?>
               </div>
-              <div class="grid-third">
-              <?php
-                
-                print "<div class=\"artikel-boks\">";
-                  print "<h2>Kontakt os, når du har brug for hjælp til hjemmesiden</h2>";
-                  print "<h3>Når du har brug for hjælp til indhold, fx tekster, placering af indhold, struktur, menuer mv.</h3>";
-                  print "<ul style=\"margin-top:0;\">";
-                  print "<li>Thomas Aagaard Kjeldsen<br />Tlf. 43 57 62 29<br /><a href=\"mailto:thk@ishoj.dk\" title=\"Send en e-mail til Thomas Aagaard Kjeldsen\">thk@ishoj.dk</a></li>";
-                  print "<li>Henrik Alexandersen<br />Tlf. 43 57 62 34<br /><a href=\"mailto:39456@ishoj.dk\" title=\"Send en e-mail til Henrik Alexandersen\">39456@ishoj.dk</a></li>";
-                  print "</ul>";
-    
-                  print "<h3>Når du har spørgsmål til teknikken, fx funktionalitet, fejlmeddelelser mv.</h3>";
-
-                  print "<ul>";
-                    print "<li>Thomas Mikkel Jensen<br />Tlf. 43 57 62 04<br /><a href=\mailto:tho@ishoj.dk\" title=\"Send en e-mail til Thomas Mikkel Jensen\">tho@ishoj.dk</a></li>";
-                    print "<li>Jsper Vig Meyer<br />Tlf. 43 57 62 03<br /><a href=\mailto:jvm@ishoj.dk\" title=\"Send en e-mail til Jesper Vig Meyer\">jvm@ishoj.dk</a></li>";
-                  print "</ul>";
-
-                print "</div>";
-              ?>       
-              </div>
+      
             </div>
           </div>
         </section>
